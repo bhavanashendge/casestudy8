bc40cbf2fa258414884000e7e18e55ff392d3764
bhavanashendge <47981943+bhavanashendge@users.noreply.github.com>
2019-03-28 16:24:26 -0400

    Update installdocker.pp

--- manifests/installdocker.pp
+++ manifests/installdocker.pp
@@ -5,7 +5,7 @@ package { 'curl':
     before => Exec['download docker']
 }
 
-exec {'apt-update':
+exec { 'apt-update':
     command => '/usr/bin/apt-get update'
  }
 
@@ -13,8 +13,11 @@ exec { 'download docker':
     command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/sudo apt-key add -'
  }
 }
-exec {'add_docker_repo':
+exec { 'add_docker_repo':
 	command => '/usr/bin/add-apt-repository "deb [arch=amd64]
 	https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
 	require => Exec['apt-update']
 	}
+exec { 'docker_cache':
+command => '/usr/bin/apt-cache policy docker-ce'
+}
