4bbc903ef879b571f4cddfcc0bd635fb3ec1272b
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-28 21:48:01 -0400

    Formatted the code.

--- manifests/installdocker.pp
+++ manifests/installdocker.pp
@@ -1,16 +1,18 @@
 class casestudy8::installdocker {
 
-package { 'curl':
-    ensure => 'installed',
+  package {
+    'curl': ensure => 'installed',
     before => Exec['download docker']
-}
+  }
 
-exec { 'download docker':
-    command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/sudo apt-key add -'
- }
+  exec {
+    'download docker': command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/sudo apt-key add -'
+  }
+
+  exec {
+    'add_docker_repo': command => '/usr/bin/add-apt-repository "deb [arch=amd64]
+    https: //download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
+      require => Exec['apt-update']
+  }
+
 }
-exec { 'add_docker_repo':
-	command => '/usr/bin/add-apt-repository "deb [arch=amd64]
-	https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
-	require => Exec['apt-update']
-	}
