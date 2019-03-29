aef341498b1a35a16c4df8383533905559a8e0c9
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-29 19:49:48 -0400

    Added additional command for policy.

--- manifests/installdocker.pp
+++ manifests/installdocker.pp
@@ -5,17 +5,23 @@ class casestudy8::installdocker {
     before => Exec['download docker']
   }
 
-  exec { 'download docker':
+  exec { 'Add docker gpg key':
     command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/sudo apt-key add -'
   }
 
-  exec { 'add_docker_repo':
+  exec { 'Add docker to APT repo':
       command => '/usr/bin/add-apt-repository "deb [arch=amd64] https: //download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
   }
 
-  exec { 'apt-get update':
+  exec { 'Apt get update':
       command => '/usr/bin/sudo apt-get update'
   }
+
+  exec { 'Check policy for docker-ce':
+      command => '/usr/bin/apt-cache policy docker-ce'
+  }
+
+
 
 
 }
