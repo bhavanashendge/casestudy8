6e0abe4aeaf7121e790b559267d29f848b053f29
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-30 09:58:48 -0400

    Fixed indentation

--- manifests/build.pp
+++ manifests/build.pp
@@ -1,23 +1,22 @@
 /*
 * Class to build docker image
+* Create a manifest to add and build a docker file to create a docker image
 */
 
 class casestudy8::build {
 
-#Create a manifest to add and build a docker file to create a docker image
-
   file {'/home/edureka/Downloads/build_repo':
         ensure => 'directory',
-        mode => '0777'
+        mode   => '0777'
   }
 
   file {'/home/edureka/Downloads/build_repo/addressbook.war':
-      mode => '0777',
+      mode   => '0777',
       source => "puppet:///modules/casestudy8/addressbook.war"
   }
 
   file {'/home/edureka/Downloads/build_repo/Dockerfile':
-      mode => '0777',
+      mode   => '0777',
       source => "puppet:///modules/casestudy8/Dockerfile"
   }
 
