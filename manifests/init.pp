1d5bfd438f6415e9f753e6a3c906c1529f91195b
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-30 01:23:03 -0400

    fixed syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class casestudy8 {
-    class {'casestudy8:: install':}
-    class {'casestudy8:: build':}
-    class {'casestudy8:: run':}
+    class {'casestudy8::install':}
+    class {'casestudy8::build':}
+    class {'casestudy8::run':}
 }
