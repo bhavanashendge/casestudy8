6c32191495d78be14574d2a79b95e3f454589bca
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-29 23:11:49 -0400

    Install Docker command

--- manifests/installdocker.pp
+++ manifests/installdocker.pp
@@ -1,24 +1,27 @@
 class casestudy8::installdocker {
-  include apt
 
   package {
     'curl': ensure => 'installed'
   }
 
-  apt::source { 'Add docker to sources':
-    comment  => 'This is the iWeb Debian unstable mirror',
-    location => 'https://download.docker.com/linux/ubuntu',
-    release  => 'stable',
-    repos    => '$(lsb_release -cs)',
-    architecture => 'amd64',
-    pin      => '-10',
-    key      => {
-      'id'     => 'A1BD8E9D78F7FE5C3E65D8AF8B48AD6246925553',
-      'server' => 'https://download.docker.com/linux/ubuntu/gpg',
-    },
-    include  => {
-      'deb' => true,
-    },
+  exec { 'Add docker gpg key':
+    command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/sudo apt-key add -'
+  }
+
+  exec { 'Add docker to APT repo':
+       command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+  }
+
+  exec { 'Apt get update':
+      command => '/usr/bin/sudo apt-get update'
+  }
+
+  exec { 'Check policy for docker-ce':
+        command => '/usr/bin/apt-cache policy docker-ce'
+  }
+
+  exec { 'Install docker-ce':
+          command => '/usr/bin/sudo apt-get install -y docker-ce'
   }
 
 }
