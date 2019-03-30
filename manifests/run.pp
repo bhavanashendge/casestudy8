d53958e4934bce0fde22bcbdc4492e15b0cca4bc
Bhavana Shendge <Bhavana.shendge@gmail.com>
2019-03-30 10:56:41 -0400

    removed . at the end

--- manifests/run.pp
+++ manifests/run.pp
@@ -1,6 +1,6 @@
 class casestudy8::run {
   exec {'Docker run':
         cwd     => '/home/edureka/Downloads/build_repo',
-        command => '/usr/bin/docker run  -it -d -p 8091:8080 addressbook .'
+        command => '/usr/bin/docker run  -it -d -p 8091:8080 addressbook'
   }
 }
